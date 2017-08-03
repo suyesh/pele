6bc3419c906dceea06bfed754eb6bd11ff486460
Suyesh Bhandari <suyeshb@gmail.com>
2017-08-02 23:15:03 -0400

    small change

--- lib/pele.rb
+++ lib/pele.rb
@@ -1,11 +1,11 @@
 require 'pele/version'
+require 'pele/utils'
 require 'thor'
 require 'os'
 require 'tty-prompt'
 require 'pele/aws_regions'
 require 'colorize'
 require 'aws-sdk'
-require 'pele/utils'
 require 'pele/cli'
 
 module Pele
--- lib/pele/version.rb
+++ lib/pele/version.rb
@@ -1,3 +1,3 @@
 module Pele
-  VERSION = "0.1.5-pre"
+  VERSION = '0.1.6-pre'.freeze
 end
