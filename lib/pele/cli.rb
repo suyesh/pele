c8b12a15476fd4bb417a47fab122b1ddc89d9ee8
Suyesh Bhandari <suyeshb@gmail.com>
2017-08-03 10:15:46 -0400

    .

--- lib/pele/cli.rb
+++ lib/pele/cli.rb
@@ -44,11 +44,14 @@ module Pele
         abort if overwrite == 'Yes'
       end
       puts ''
-      key_pair_name = prompt.ask('Name your key-pair. For example: mykeypair.colorize(:green)')
+      key_pair = prompt.ask('Name your key-pair. For example: mykeypair.colorize(:green)')
       begin
         current_dirname = File.basename(Dir.getwd)
         ec2 = Aws::EC2::Client.new
-        ec2.create_key_pair(key_name: key_pair_name)
+        ec2.create_key_pair(key_name: key_pair)
+        create_file "~/.ssh/#{key_pair}.pem" do
+          key_pair.key_fingerprint
+        end
         File.open("~/.ssh/#{key_pair_name}.pem", 'w') do |file|
           file.write key_pair_name
         end
